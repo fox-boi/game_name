c818a5af9e80f62ac9ef8d86f0a6d5ab83edce9d
fox-boi <cmc1102012@gmail.com>
2025-01-16 23:12:02 +0000

    yoo

--- api/index.py
+++ api/index.py
@@ -1,12 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'test = input(yo) print(test)'
-
-
-@app.route('/about')
-def about():
-    return 'About'
+test = input("what?")
+print()
+print(test)
